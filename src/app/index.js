2f18b008ff440c225e92235c0629cfb6d15f4a37
Mikle <mikle.blv@gmail.com>
2016-06-01 21:43:03 +0300

    - added template for new calendar

--- src/app/index.js
+++ src/app/index.js
@@ -3,10 +3,15 @@
 angular.module('keymaker', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'ngMessages'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
+      .state('template', {
+        url: 'template',
         templateUrl: 'app/main/main.html',
         controller: 'FormCtrl'
+      })
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/viewYears.html',
+        controller: 'ViewYearsController'
       });
 
     $urlRouterProvider.otherwise('/');
